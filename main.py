97ee12f86ece078330b4359565229fc9f23e7900
Matt <MGetchell11@yahoo.com>
2020-12-21 15:47:52 -0500

    Removed pop-up windows and added in password generator button

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from tkinter import *
+import string
+import secrets
 
-def screen_2():
-    second_screen = Tk()
-    second_screen.geometry("300x250")
-    second_screen.title("Passwords Updated!")
+# Display randomly generated password
+def password_gen():
+    alphabet = string.ascii_letters + string.digits + '-_!*'
+    while True:
+        password = ''.join(secrets.choice(alphabet) for i in range(20))
+        if (any(c.islower() for c in password) >= 4
+                and sum(c.isupper() for c in password) >= 4
+                and sum(c.isdigit() for c in password) >= 4):
+            break
+        Label(text=password, font="Calibri").pack()
+        break
+
+# Display passwords
+def password_list():
     Label(text="gmail = 123456\nyahoo = 98765\nspotify = password123\nsteam = 13579", font="Calibri").pack()
-def screen_1():
+
+# Start Screen with buttons to redirect
+def main_screen():
     first_screen = Tk()
     first_screen.geometry("300x250")
     first_screen.title("Password Manager")
-    Button(text="Get Passwords", height="2", width="30", command=screen_2).pack()
+    Button(text="Get Passwords", height="2", width="30", command=password_list).pack()
+    Button(text="Generate Random Password", height="2", width="30", command=password_gen).pack()
     first_screen.mainloop()
-screen_1()
+main_screen()
